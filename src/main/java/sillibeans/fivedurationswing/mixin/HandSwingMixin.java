4072f72cbf83c925e31c843f147d61457eb7301d
Cas Pascal <casboi86@gmail.com>
2026-06-21 07:16:14 +0700

    Fix interacting animation not keeping up while holding right mouse button

--- src/main/java/sillibeans/fivedurationswing/mixin/HandSwingMixin.java
+++ src/main/java/sillibeans/fivedurationswing/mixin/HandSwingMixin.java
@@ -5,46 +5,73 @@ import net.minecraft.client.option.GameSettings;
 import net.minecraft.core.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(Player.class)
 public class HandSwingMixin {
-	/**
-	 * @author sillibeanss
-	 * @reason the duration is hardcoded so the only way to change it is to overwrite the method.
-	 */
-	@Overwrite
-	protected void updateAI() {
-		Player p = (Player) (Object) this;
-		Minecraft client = Minecraft.getMinecraft();
-		final boolean isContinuous = GameSettings.KEY_ATTACK.isPressed() && client.objectMouseOver != null;
+	@Unique
+	private final static int IDLE_DURATION = 5;
 
-		final int maxDuration = 6;
-		final int duration = isContinuous ? 3 : maxDuration;
+	@Unique
+	private final static int INTERACT_DURATION = 4;
 
-		if (p.isSwinging) {
-			p.swingProgressInt += 1;
+	@Unique
+	private final static int MINE_DURATION = 3;
 
-			if (p.swingProgressInt >= duration) {
-				p.swingProgressInt = 0;
+	@Unique
+	private int progress = 0;
 
-				if (!isContinuous) {
-					p.isSwinging = false;
-				}
+	@Unique
+	private int computeDuration(boolean isLookingAtObj, boolean isMining, boolean isInteracting) {
+		if (isLookingAtObj) {
+			if (isMining) {
+				return MINE_DURATION;
 			}
-		} else {
-			p.swingProgressInt = 0;
+
+			if (isInteracting) {
+				return INTERACT_DURATION;
+			}
 		}
 
-		p.swingProgress = (float)p.swingProgressInt / (float) maxDuration;
+		return IDLE_DURATION;
 	}
 
 	/**
 	 * @author sillibeanss
-	 * @reason disallow swingProgressInt being forced to -1
+	 * @reason the duration is hardcoded so the only way to change it is to overwrite the method.
 	 */
+
+	// This is much harder to implement because the animation is fully dependent on swingItem(),
+	// while having no extra info on whether the user is holding down the mouse buttons or not.
+	// This adds 3 different durations for idle (5), interacting (4) and mining (3).
+	// The name "Five Duration Swing" refers to the modern minecraft swing animation
+	// implementation, where a swing speed of 5 makes the animation run at 3 ticks
+	// while mining (for some reason).
 	@Overwrite
-	public void swingItem() {
+	protected void updateAI() {
 		Player p = (Player) (Object) this;
-		p.isSwinging = true;
+		Minecraft client = Minecraft.getMinecraft();
+		final boolean isLookingAtObj = client.objectMouseOver != null;
+		final boolean isMining = GameSettings.KEY_ATTACK.isPressed();
+		final boolean isInteracting = GameSettings.KEY_INTERACT.isPressed();
+
+		final int duration = computeDuration(isLookingAtObj, isMining, isInteracting);
+
+		if (p.isSwinging) {
+			progress += 1;
+
+			if (progress >= duration) {
+				progress = 0;
+
+				if (!(isLookingAtObj && (isMining || isInteracting))) {
+					p.isSwinging = false;
+				}
+			}
+		} else {
+			progress = 0;
+		}
+
+		p.swingProgressInt = progress;
+		p.swingProgress = (float)p.swingProgressInt / (float) IDLE_DURATION;
 	}
 }
